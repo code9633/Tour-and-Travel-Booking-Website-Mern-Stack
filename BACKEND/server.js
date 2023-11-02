992ee629de1673dcad9398ba49f8e9647f80c151
code9633 <shashikaudara5566@gmail.com>
2023-11-02 15:33:23 +0530

    first commit

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -9,3 +9,17 @@ const PORT = process.env.PORT || 8070
 
 app.use(cors())
 app.use(bodyParser.json())
+
+mongoose.connect(URL,{
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+})
+
+const connection  =  mongoose.connection
+connection.once('open', ()=>{
+    console.log("Mongodb connection sucess!!")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is up ans running on port number ":${PORT}`)
+})
